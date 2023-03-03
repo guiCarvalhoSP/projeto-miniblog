16d87f08da25d018008acf6f990a22ea41234039
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-03 13:59:07 -0300

    Inicio dos formularios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Navbar from './components/Navbar';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
@@ -15,6 +19,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
           </Routes>
         </div>
         <Footer />
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,53 @@
+import styles from './Register.module.css';
+
+import { useState, useEffect } from 'react';
+
+const Register = () => {
+  return (
+    <div className={styles.register}>
+      <h1>Cadastre-se</h1>
+      <p>Crie seu usuário e compartilhe suas histórias</p>
+      <form>
+        <label>
+          <span>Nome:</span>
+          <input 
+            type="text" 
+            name='displayName' 
+            required 
+            placeholder='Nome do usuário'/>
+        </label>
+
+        <label>
+          <span>Email:</span>
+          <input 
+            type="email" 
+            name='email' 
+            required 
+            placeholder='Email do usuário'/>
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input 
+            type="password" 
+            name='password' 
+            required 
+            placeholder='Insira sua senha'/>
+        </label>
+
+        <label>
+          <span>Confirme sua senha:</span>
+          <input 
+            type="password" 
+            name='confirmPassword' 
+            required 
+            placeholder='Insira novamente sua senha'/>
+        </label>
+
+        <button className='btn'>Cadastrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
