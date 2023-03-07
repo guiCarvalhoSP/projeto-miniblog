405179cda4c4ea7cbdf230faae1fe494edc884bd
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2023-03-07 14:51:12 -0300

    Criação da pagina home
    Hook para resgate de dados

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,33 @@
 import styles from './Home.module.css';
+
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+
 const Home = () => {
+
+  const [query, setQuery] = useState('');
+  const [posts] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>
-        <h1>Home</h1>
+    <div className={styles.home}>
+        <h1>Veja os nossos posts mais recentes</h1>
+        <form onSubmit={handleSubmit} className={styles.search_form}>
+          <input type="text" placeholder='Busque por tags...' onChange={(e) => setQuery(e.target.value)}/>
+          <button className='btn btn-dark'>Pesquisar</button>
+        </form>
+
+        <div>
+          {posts && posts.length === 0 && (
+            <div className={styles.noposts}>
+              <p>Não foram encontrados posts, seja o primeiro a postar!</p>
+              <Link className="btn" to="/posts/create">Criar primeiro post</Link>
+            </div>
+          )}
+        </div>
     </div>
   )
 }
